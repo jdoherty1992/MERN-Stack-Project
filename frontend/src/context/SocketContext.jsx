953891b52b0b911d227d9681dd253907fb63aeb3
James Doherty <j_doherty1992@hotmail.com>
2025-04-03 20:25:17 +0100

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://mern-stack-project-22m1.onrender.com/", {
                 query:{
                     userId: authUser._id,
                 },
