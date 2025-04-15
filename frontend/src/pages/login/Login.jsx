d07a897161f84d96e7e0da857c30bed052f8c1c4
James Doherty <j_doherty1992@hotmail.com>
2025-04-15 18:10:02 +0100

    Updated Version

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -17,9 +17,9 @@ const Login = () => {
   return (
   <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
     <div className='w-full p-6 rounded-1g shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-      <h1 className= 'text-3xl font-semibold text-center text-gray-300'>
+      <h1 className= 'text-3xl font-semibold text-center text-gray-200'>
         Login
-      <span className='text-blue-500'> ChatApp</span>       
+      <span className='text-blue-500'> StackChat</span>       
       </h1>
 
       <form onSubmit={handleSubmit}>
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -27,8 +27,8 @@ const SignUp = () => {
   return (
   <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
     <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-      <h1 className='text-3xl font-semibold text-center text-gray-300'>
-        Sign Up <span className='text-blue-500'> ChatApp</span>
+      <h1 className='text-3xl font-semibold text-center text-gray-200'>
+        Sign Up <span className='text-blue-500'> StackChat</span>
       </h1>
 
       <form onSubmit={handleSubmit}>
