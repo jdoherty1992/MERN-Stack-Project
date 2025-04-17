0981dd560eb22f94483a053024ea6aebb00d3ab6
James Doherty <j_doherty1992@hotmail.com>
2025-04-17 16:30:35 +0100

    updated version

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -9,7 +9,7 @@ const Message = ({ message }) => {
 	const formattedTime = extractTime(message.createdAt);
 	const chatClassName = fromMe ? "chat-end" : "chat-start";
 	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
-	const bubbleBgColor = fromMe ? "bg-green-300 text-black" : "";
+	const bubbleBgColor = fromMe ? "bg-green-300 text-black !text-black" : "";
 
 	const shakeClass = message.shouldShake ? "shake" : "";
 
