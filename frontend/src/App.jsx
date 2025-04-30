5b27665bab7d7690670d682a09296815b5672d7e
James Doherty <j_doherty1992@hotmail.com>
2025-05-01 00:35:01 +0100

    Updated Version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import SignUp from './pages/signup/SignUp';
-import Profile from './pages/profile/profile';
+import Profile from './pages/profile/Profile';
 import { Toaster } from 'react-hot-toast'; 
 import { useAuthContext } from './context/AuthContext';
 import UpdateProfile from './pages/updateProfile/UpdateProfile';
